31e1923a38d8511c1aa6304aea3392079098e680
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-07-27 14:28:04 +0200

    modified batch command

--- src/Neoxygen/UpDown/Command/Node/batchNodeCreate.php
+++ src/Neoxygen/UpDown/Command/Node/batchNodeCreate.php
@@ -8,37 +8,27 @@ use Guzzle\Service\Command\AbstractCommand;
 /**
  * Sends an API call to find a node in the database
  *
- * @guzzle properties doc="an array of key values pairs defining the node properties"
  */
 class batchNodeCreate extends AbstractCommand
 {
 
-    protected $commands = array();
+    protected $batch = array();
     
+    protected $method = 'POST';
     
-	/**
-	 * If an empty array is passed, the effect will be the same
-	 * as if the createEmptyNode command was called
-	 */
-	public function setCommand($command)
-	{
-		$this->commands[] = $command;
-	}
+    public function addNode(array $properties)
+    {
+        $this->batch[] = array('method' => 'POST', 'to' => '/node', 'body' => $properties);
+    }
 
 	protected function build()
 	{
-            foreach($this->commands as $command)
-            {
-                $method = $command->getRequest()->getMethod();
-                var_dump($method);
-                exit();
-            }
             
             
             
 		$uri = $this->client->getUriForAction('batch');
 		$this->request = $this->client->post(array($uri, $this->data));
-		$this->request->setBody(json_encode($this->get('properties')));
+		$this->request->setBody(json_encode($this->batch));
 		$this->request->setHeader('Accept', 'application/json');
 		$this->request->setHeader('Content-Type', 'application/json');
 	}
