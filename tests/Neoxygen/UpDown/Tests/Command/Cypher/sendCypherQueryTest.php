e8e338b07f1de6884dd1fee83c071e042a327129
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-11 01:38:15 +0200

    added a note on how to get the raw http request

--- tests/Neoxygen/UpDown/Tests/Command/Cypher/sendCypherQueryTest.php
+++ tests/Neoxygen/UpDown/Tests/Command/Cypher/sendCypherQueryTest.php
@@ -10,13 +10,17 @@ class sendCypherQueryTest extends Guzzle\Tests\GuzzleTestCase
 		$client = $this->getServiceBuilder()->get('test.updown');
 		$command = $client->getCommand('Cypher\sendCypherQuery');
 		$node = array('name' => 'Angus Young', '_uid' => uniqid());
-                $query = 'CREATE n={node} return n;';
+                $query = 'CREATE n={node} return n';
                 $params = array('node' => $node);
                 $command->setQuery($query);
                 $command->setParameters($params);
 
 		$execute = $client->execute($command);
 		$result = $command->getResult();
+
+		// You can get the raw http request with the following code :
+		// $rbody = $command->getRequest();
+		// print_r($rbody->__toString());
 
 		$this->assertTrue(array_key_exists('columns', $result));
                 $this->assertTrue(array_key_exists('data', $result));
