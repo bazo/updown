1f698ae6fa8af17bee9fb922c25797115b591c28
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-11 02:00:43 +0200

    fixed shell

--- bin/ci/start_neo4j_server.sh
+++ bin/ci/start_neo4j_server.sh
@@ -2,6 +2,5 @@
 
 # travis-ci.org provides neo4j server but it will be disabled from starting on boot
 # in the future
-which neo4j && sudo neo4j start
+which neo4j && sudo neo4j info && sudo neo4j start
 sleep 3
-neo4j info
