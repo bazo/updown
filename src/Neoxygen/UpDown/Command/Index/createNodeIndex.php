d4398c4832034b7b14273ead6265d3ea73130aaa
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-13 11:05:28 +0200

    added index config options

--- src/Neoxygen/UpDown/Command/Index/createNodeIndex.php
+++ src/Neoxygen/UpDown/Command/Index/createNodeIndex.php
@@ -9,9 +9,20 @@ use Guzzle\Service\Command\AbstractCommand;
  * Sends an API call to find a node in the database
  *
  * @guzzle name doc="the index name" required="true"
+ * @guzzle config doc="configuration options for custom index"
  */
 class createNodeIndex extends AbstractCommand
 {
+
+	public function setConfig(array $config)
+	{
+		foreach ($config as $key => $value){
+			if (empty($value)){
+				return false;
+			}
+		}
+		$this->set('config', $config);
+	}
 
 	public function setName($name)
 	{
@@ -20,10 +31,9 @@ class createNodeIndex extends AbstractCommand
 
 	protected function build()
 	{
-		$body = array('name' => $this->get('name'));
 		$uri = $this->client->getUriForAction('node_index');
 		$this->request = $this->client->post(array($uri, $this->data));
-		$this->request->setBody(json_encode($body));
+		$this->request->setBody(json_encode($this->getCreateBody()));
 		$this->request->setHeader('Accept', 'application/json');
 		$this->request->setHeader('Content-Type', 'application/json');
 	}
@@ -31,5 +41,15 @@ class createNodeIndex extends AbstractCommand
 	public function getResult()
 	{
 		return parent::getResult();
+	}
+
+	private function getCreateBody()
+	{
+		$body = array();
+		$body['name'] = $this->get('name');
+		if(null !== $this->get('config', null)){
+			$body['config'] = $this->get('config');
+		}
+		return $body;
 	}
 }
